47a536495f94bb0828243972412fe565bce91795
NotableDeveloper <castlereal01@gmail.com>
2023-04-17 16:53:51 +0900

    Repository init 레포지토리 생성 및 초기화
    Spring 2.7.11, Java 11 사용

---
+++ src/main/java/NotableDeveloper/rank/RankApplication.java
@@ -0,0 +1,13 @@
+package NotableDeveloper.rank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NotableDeveloper/rank/RankApplicationTests.java
@@ -0,0 +1,13 @@
+package NotableDeveloper.rank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
