beadebdaedce8c8bba23aa9b929550b5fb5d2b92
NotableDeveloper <castlereal01@gmail.com>
2023-04-25 00:11:32 +0900

    setUp 메서드 추가 BeforeEach 어노테이션을 사용하여 테스트를 위한 객체를 준비하는 메서드 작성. 이에 따라, 단일 강의를 등록하는 테스트 수정하였음. 이후로는 여러 분반으로 개설된 강의를 등록하는 테스트를 작성할 예정.

--- src/test/java/NotableDeveloper/rank/CourseRepositoryTest.java
+++ src/test/java/NotableDeveloper/rank/CourseRepositoryTest.java
@@ -1,35 +1,59 @@
 package NotableDeveloper.rank;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
 
 @DataJpaTest
 public class CourseRepositoryTest {
     @Autowired
     CourseRepository courseRepository;
 
+    static ArrayList<Course> courses;
+
+    @BeforeEach
+    @DisplayName("3개의 분반이 개설된 객체지향 프로그래밍 강의를 준비한다")
+    void setUp(){
+        courses = new ArrayList<>();
+
+        courses.add(new Course("객체지향 프로그래밍", 2023, Semester.FIRST, "12345678"));
+        courses.add(new Course("객체지향 프로그래밍", 2023, Semester.FIRST, "12345678"));
+        courses.add(new Course("객체지향 프로그래밍", 2023, Semester.FIRST, "12345678"));
+    }
     @Test
+    @DisplayName("객체지향 프로그래밍 강의를 하나 등록한다")
     public void 강의_정상등록_테스트() {
-        final String title = "객체지향 프로그래밍";
-        final int offeredYear = 2023;
-        final Semester semester = Semester.FIRST;
-        final String code = "1234567890";
+        Course targetCourse = courses.get(0);
 
-        final Course course = new Course(title, offeredYear, semester, code);
+        courseRepository.save(targetCourse);
 
-        courseRepository.save(course);
-
-        final Course findCourse = courseRepository.findByTitleAndOfferedYearAndSemester(title, offeredYear, semester);
+        final Course findCourse = courseRepository.findByTitleAndOfferedYearAndSemester("객체지향 프로그래밍", 2023, Semester.FIRST);
 
         Assertions.assertEquals("객체지향 프로그래밍", findCourse.getTitle());
         Assertions.assertEquals(2023, findCourse.getOfferedYear());
         Assertions.assertEquals(Semester.FIRST, findCourse.getSemester());
-        Assertions.assertEquals(code, findCourse.getCode());
+        Assertions.assertEquals("12345678", findCourse.getCode());
         Assertions.assertEquals(0.0F, findCourse.getRating());
         Assertions.assertEquals(Tier.F, findCourse.getTier());
         Assertions.assertEquals(1, findCourse.getCount());
+    }
+
+    @Test
+    @DisplayName("여러 분반으로 개설되는 강의는 DB에 하나만 등록되어야 한다")
+    public void 여러분반_강의등록_테스트(){
+        for(Course course : courses){
+            /*
+                To Do :
+                등록하려는 강의가 이미 DB 내에 있는 지를 확인하는 코드 작성하기
+                (DB 내에 강의명, 년도, 학기, 과목 코드가 동일한 것이 있는 지를 확인하는 메서드 작성)
+             */
+            courseRepository.save(course);
+        }
+
     }
 }
