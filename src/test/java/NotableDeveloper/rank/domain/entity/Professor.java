c262b87cbf5fa44da582fd73934177957721ba94
NotableDeveloper <castlereal01@gmail.com>
2023-05-17 02:03:04 +0900

    Professor Entity 최초 작성 Professor Entity 클래스 최초 작성.

---
+++ src/test/java/NotableDeveloper/rank/domain/entity/Professor.java
@@ -0,0 +1,35 @@
+package NotableDeveloper.rank.domain.entity;
+
+import NotableDeveloper.rank.domain.enums.Tier;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+public class Professor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Department department;
+
+    @Column(nullable = false)
+    private String position;
+
+    @Column(nullable = false)
+    private float rating;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Tier tier;
+}
--- src/test/java/NotableDeveloper/rank/repository/ProfessorRepositoryTest.java
+++ src/test/java/NotableDeveloper/rank/repository/ProfessorRepositoryTest.java
@@ -1,7 +1,10 @@
 package NotableDeveloper.rank.repository;
 
+import NotableDeveloper.rank.domain.entity.Professor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
 
 @DataJpaTest
 public class ProfessorRepositoryTest {
