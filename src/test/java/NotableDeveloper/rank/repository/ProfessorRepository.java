7e95a6a8d2e3108bf08699518d58a07279e77209
NotableDeveloper <castlereal01@gmail.com>
2023-05-17 02:15:32 +0900

    ProfessorRepository 상속 수정, DepartmentRepository 작성 ProfessorRepository의 상속 부분 수정. DepartmentRepository 최초 작성.

--- src/test/java/NotableDeveloper/rank/repository/ProfessorRepository.java
+++ src/test/java/NotableDeveloper/rank/repository/ProfessorRepository.java
@@ -1,6 +1,7 @@
 package NotableDeveloper.rank.repository;
 
+import NotableDeveloper.rank.domain.entity.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProfessorRepository extends JpaRepository {
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 }
