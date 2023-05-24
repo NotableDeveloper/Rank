8d09ece40f227bee5f8e9961b25e96bd5c7cf81a
NotableDeveloper <castlereal01@gmail.com>
2023-05-24 15:08:24 +0900

    CourseProfessorRepository 테스트 작성 CourseRepository Entity, Repository 작성을 위한 테스트 코드 작성.

---
+++ src/test/java/NotableDeveloper/rank/CourseProfessorRepositoryTest.java
@@ -0,0 +1,40 @@
+package NotableDeveloper.rank;
+
+import NotableDeveloper.rank.domain.entity.Course;
+import NotableDeveloper.rank.domain.entity.Department;
+import NotableDeveloper.rank.domain.entity.Professor;
+import NotableDeveloper.rank.repository.CourseRepository;
+import NotableDeveloper.rank.repository.DepartmentRepository;
+import NotableDeveloper.rank.repository.ProfessorRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+
+@DataJpaTest
+public class CourseProfessorRepositoryTest {
+    @Autowired
+    CourseRepository courseRepository;
+
+    @Autowired
+    ProfessorRepository professorRepository;
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+
+    @Autowired
+    CourseProfessorRepository courseProfessorRepository;
+
+    static ArrayList<Course> courses;
+    static ArrayList<Professor> professors;
+    static ArrayList<Department> departments;
+
+
+    @BeforeEach
+    void setUp(){
+        courses = new ArrayList<>();
+        professors = new ArrayList<>();
+        departments = new ArrayList<>();
+    }
+}
