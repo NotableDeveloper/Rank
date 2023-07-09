fc0195a2304a941b8a6c59b04ed20092412e72c3
NotableDeveloper <castlereal01@gmail.com>
2023-07-10 07:47:53 +0900

    강의 아이디 검색 기능 구현 강의를 아이디로 검색하는 기능 구현 및 테스트 작성.

--- src/test/java/NotableDeveloper/rank/controller/CourseController.java
+++ src/test/java/NotableDeveloper/rank/controller/CourseController.java
@@ -1,5 +1,6 @@
 package NotableDeveloper.rank.controller;
 
+import NotableDeveloper.rank.domain.request.GetCourseResponse;
 import NotableDeveloper.rank.domain.response.GetCoursesResponse;
 import NotableDeveloper.rank.service.CourseService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/course")
@@ -39,6 +37,24 @@ public class CourseController {
 
         } catch (Exception e){
            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/{courseId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity searchCourseByCourseId(@PathVariable int courseId){
+        try{
+            GetCourseResponse response = GetCourseResponse.builder()
+                    .detailCourse(courseService.getCourseById((long) courseId))
+                    .build();
+
+            String responseBody = objectMapper.writeValueAsString(response);
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(responseBody);
+
+        } catch (Exception e){
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- src/test/java/NotableDeveloper/rank/test/controller/CourseControllerTest.java
+++ src/test/java/NotableDeveloper/rank/test/controller/CourseControllerTest.java
@@ -1,13 +1,19 @@
 package NotableDeveloper.rank.test.controller;
 
 import NotableDeveloper.rank.controller.CourseController;
+import NotableDeveloper.rank.domain.dto.CourseDetailDto;
 import NotableDeveloper.rank.domain.dto.CourseDto;
+import NotableDeveloper.rank.domain.dto.CourseHistoryDto;
+import NotableDeveloper.rank.domain.dto.ProfessorDto;
 import NotableDeveloper.rank.domain.entity.Course;
 import NotableDeveloper.rank.domain.entity.CourseProfessor;
+import NotableDeveloper.rank.domain.entity.Professor;
 import NotableDeveloper.rank.domain.exceptiion.CourseNotFoundException;
+import NotableDeveloper.rank.domain.request.GetCourseResponse;
 import NotableDeveloper.rank.domain.response.GetCoursesResponse;
 import NotableDeveloper.rank.service.CourseService;
 import NotableDeveloper.rank.test.data.RankData;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -88,5 +94,66 @@ public class CourseControllerTest {
                 .andExpect(content().json(responseBody));
 
         verify(courseService).getCourseByTitle(title);
+    }
+
+    @Test
+    void 강의_아이디_검색_성공_테스트() throws Exception {
+        int courseId = 1;
+
+        CourseProfessor findByCourseId = data.getCourseProfessors().stream()
+                .filter(cp -> cp.getCourse().getId().equals((long) courseId))
+                .findFirst()
+                .orElse(null);
+
+        Course c = findByCourseId.getCourse();
+        Professor p = findByCourseId.getProfessor();
+
+        List<Course> findAllByCourseCode = data.getCourseProfessors().stream()
+                .filter(cp -> cp.getCourse().getCode() == c.getCode())
+                .map(cp -> {
+                    Course course = cp.getCourse();
+                    return course;
+                }).collect(Collectors.toList());
+
+        List<CourseHistoryDto> history = findAllByCourseCode.stream()
+                .map(course -> CourseHistoryDto.builder()
+                        .courseId(course.getId())
+                        .tier(course.getTier())
+                        .year(course.getOfferedYear())
+                        .semester(course.getSemester())
+                        .build()
+                ).collect(Collectors.toList());
+
+        ProfessorDto professorDto = ProfessorDto.builder()
+                .professorId(p.getId())
+                .name(p.getName())
+                .department(p.getDepartment().getOriginalName())
+                .position(p.getPosition())
+                .tier(p.getTier())
+                .build();
+
+        CourseDetailDto courseDetailDto = CourseDetailDto.builder()
+                .courseId(c.getId())
+                .code(c.getCode())
+                .semester(c.getSemester())
+                .year(c.getOfferedYear())
+                .courseTier(c.getTier())
+                .professor(professorDto)
+                .history(history)
+                .build();
+
+        String responseBody = objectMapper.writeValueAsString(
+                GetCourseResponse.builder()
+                        .detailCourse(courseDetailDto)
+                        .build());
+
+        Mockito.when(courseService.getCourseById((long) courseId))
+                .thenReturn(courseDetailDto);
+
+        mockMvc.perform(get("/course/{courseId}", courseId))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseBody));
+
+        verify(courseService).getCourseById((long) courseId);
     }
 }
