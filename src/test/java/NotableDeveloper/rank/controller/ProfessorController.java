949f4dd52d1adcc1ee4ee1502e35522ff697f5b2
NotableDeveloper <castlereal01@gmail.com>
2023-07-10 17:18:52 +0900

    교수이름 검색 기능 구현 교수를 이름으로 검색하는 기능을 구현함. 실패와 성공 테스트 작성 완료.

--- src/test/java/NotableDeveloper/rank/controller/ProfessorController.java
+++ src/test/java/NotableDeveloper/rank/controller/ProfessorController.java
@@ -1,6 +1,7 @@
 package NotableDeveloper.rank.controller;
 
 import NotableDeveloper.rank.domain.dto.ProfessorDto;
+import NotableDeveloper.rank.domain.response.GetProfessorsResponse;
 import NotableDeveloper.rank.service.ProfessorService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
@@ -10,12 +11,13 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/professors")
+@RequestMapping("/professor")
 @Setter
 @Getter
 public class ProfessorController {
@@ -35,6 +37,24 @@ public class ProfessorController {
                     .body(responseBody);
 
         } catch (Exception e){
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity searchProfessorsByName(@RequestParam String name){
+        try{
+            GetProfessorsResponse response = GetProfessorsResponse.builder()
+                    .professors(professorService.getProfessorsByName(name))
+                    .build();
+
+            String responseBody = objectMapper.writeValueAsString(response);
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(responseBody);
+
+        } catch(Exception e){
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
--- src/test/java/NotableDeveloper/rank/test/controller/ProfessorControllerTest.java
+++ src/test/java/NotableDeveloper/rank/test/controller/ProfessorControllerTest.java
@@ -2,9 +2,13 @@ package NotableDeveloper.rank.test.controller;
 
 import NotableDeveloper.rank.controller.ProfessorController;
 import NotableDeveloper.rank.domain.dto.ProfessorDto;
+import NotableDeveloper.rank.domain.entity.Course;
+import NotableDeveloper.rank.domain.entity.Professor;
+import NotableDeveloper.rank.domain.exceptiion.ProfessorNotFoundException;
 import NotableDeveloper.rank.domain.response.GetProfessorsResponse;
 import NotableDeveloper.rank.service.ProfessorService;
 import NotableDeveloper.rank.test.data.RankData;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -12,16 +16,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.ResetMocksTestExecutionListener;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProfessorController.class)
@@ -48,5 +52,54 @@ public class ProfessorControllerTest {
                 .andExpect(status().isOk());
 
         verify(professorService).getHonorProfessors();
+    }
+
+    @Test
+    void 교수_검색_실패_테스트() throws Exception{
+        String name = "@#!#@$";
+
+        doThrow(ProfessorNotFoundException.class)
+                .when(professorService)
+                .getProfessorsByName(name);
+
+        mockMvc.perform(get("/professor")
+                .param("name", name))
+                .andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    void 교수명_검색_성공_테스트() throws Exception {
+        String name = "철수";
+
+        List<ProfessorDto> findAllByName = data.getCourseProfessors()
+                .stream()
+                .filter(cp -> cp.getProfessor().getName().equals(name))
+                .map(cp -> {
+                    Professor p = cp.getProfessor();
+
+                    return ProfessorDto.builder()
+                            .professorId(p.getId())
+                            .tier(p.getTier())
+                            .name(p.getName())
+                            .position(p.getPosition())
+                            .department(p.getDepartment().getOriginalName())
+                            .build();
+                })
+                .collect(Collectors.toList());
+
+        Mockito.when(professorService.getProfessorsByName(name))
+                .thenReturn(findAllByName);
+
+        String responseBody = objectMapper.writeValueAsString(
+                GetProfessorsResponse.builder()
+                        .professors(findAllByName)
+                        .build());
+
+        mockMvc.perform(get("/professor")
+                .param("name", name))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseBody));
+
+        verify(professorService).getProfessorsByName(name);
     }
 }
